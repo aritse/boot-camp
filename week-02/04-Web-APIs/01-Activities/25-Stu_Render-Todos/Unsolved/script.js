ba1b8d66ce9b4421c6c8c579253b7950842851dd
Ari Tserendavaa <tserenda@hotmail.com>
2019-12-06 08:54:43 -0800

    work in progress

--- week-02/04-Web-APIs/01-Activities/25-Stu_Render-Todos/Unsolved/script.js
+++ week-02/04-Web-APIs/01-Activities/25-Stu_Render-Todos/Unsolved/script.js
@@ -4,3 +4,21 @@ var todoList = document.querySelector("#todo-list");
 var todoCountSpan = document.querySelector("#todo-count");
 
 var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
+var count = 0;
+
+todoInput.addEventListener("change", function() {
+  event.preventDefault();
+  var todoItem = document.createElement("li");
+  var completeButton = document.createElement("button");
+  completeButton.textContent = "Complete";
+  todoItem.textContent = event.target.value;
+  todoList.appendChild(todoItem);
+  count++;
+  todoItem.appendChild(complesteButton);
+  event.target.value = "";
+  todoCountSpan.textContent = count;
+});
+
+todoForm.addEventListener("submit", function() {
+  event.preventDefault();
+});
