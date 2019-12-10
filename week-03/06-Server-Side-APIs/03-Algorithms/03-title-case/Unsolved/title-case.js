551dcf259ed63c985a3d93ab9a0e584053089611
Joseph Rehfuss <josephrehfuss@D-172-22-159-49.dhcp4.washington.edu>
2019-12-10 14:52:14 -0800

    adding tuesday solutions

--- week-03/06-Server-Side-APIs/03-Algorithms/03-title-case/Unsolved/title-case.js
+++ week-03/06-Server-Side-APIs/03-Algorithms/03-title-case/Unsolved/title-case.js
@@ -1,3 +1,20 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
-var titleCase = function(str) {};
+var titleCase = function(str) {
+    var strArr = str.split(" ");
+    var resultsArr = [];
+    for (let i = 0; i < strArr.length; i++) {
+        // console.log(strArr[i][0].toUpperCase())
+        // var thisWord = strArr[i];
+        // thisWord[0] = thisWord[0].toUpperCase();
+        // console.log(thisWord);
+        var thisWordArr = strArr[i].split('');
+        console.log(thisWordArr);
+        thisWordArr[0] = thisWordArr[0].toUpperCase();
+        resultsArr.push(thisWordArr.join(""));
+        console.log(resultsArr);
+    }
+    var result = resultsArr.join(' ');
+    console.log(result);
+    return result;
+};
