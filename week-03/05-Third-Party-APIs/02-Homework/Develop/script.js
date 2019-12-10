876523d03e64ebbaffbc87e622350c28f926e223
Ari Tserendavaa <tserenda@hotmail.com>
2019-12-10 13:45:47 -0800

    finalized the app

--- week-03/05-Third-Party-APIs/02-Homework/Develop/script.js
+++ week-03/05-Third-Party-APIs/02-Homework/Develop/script.js
@@ -1,12 +1,10 @@
 const hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-
 const container = $(".container");
 
 function loadPage() {
   console.log("loading page...");
   hours.forEach(hourBlock => {
     var hourBlock = $("<div>");
-
     container.append(hourBlock);
   });
 }
