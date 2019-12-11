735c643bb22bc3eba807bd48ef3ee252ce3f54ba
Ari Tserendavaa <tserenda@hotmail.com>
2019-12-10 21:43:46 -0800

    completed the logic

--- week-03/05-Third-Party-APIs/02-Homework/Develop/script.js
+++ week-03/05-Third-Party-APIs/02-Homework/Develop/script.js
@@ -1,13 +1,37 @@
 const hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-const container = $(".container");
 
 function loadPage() {
-  console.log("loading page...");
-  hours.forEach(hourBlock => {
-    var hourBlock = $("<div>");
-    var hour;
-    container.append(hourBlock);
+  hours.forEach(hour => {
+    var timeblockDiv = $("<div>").attr({ class: "timeblock-div", "data-time": hour });
+    var hourSpan = $("<span>")
+      .text(hour)
+      .addClass("hour-span");
+    var eventInput = $("<input>").attr("type", "text");
+    eventInput.val(localStorage.getItem(hour));
+    var save = $("<button>")
+      .text("Save")
+      .attr({ type: "button", id: "save-button" });
+    timeblockDiv.append(hourSpan, eventInput, save);
+    $(".container").append(timeblockDiv);
   });
 }
 
+function handleSave(event) {
+  event.preventDefault();
+  var saveButton = event.target;
+  var eventInput = saveButton.previousSibling.value;
+  var hourSpan = saveButton.previousSibling.previousSibling.innerText;
+  localStorage.setItem(hourSpan, eventInput);
+}
+
+$(document).on("click", "#save-button", handleSave);
+
 loadPage();
+
+/*
+  <div>
+  <span>09:00</span>
+  <input type="text">
+  <button>Save</button>
+</div>
+*/
