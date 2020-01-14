058a86a5b70813ba4ebc001963ff274cad2481c8
Ari Tserendavaa <tserenda@hotmail.com>
2020-01-14 11:20:26 -0800

    solved

---
+++ week-07/13-MVC/01-Activities/03-SeinfeldApp/show.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "shows"
+});
+
+connection.connect(err => {
+  if (err) throw err;
+  console.log("db server connected as", connection.threadId);
+});
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get("/cast", (req, res) => {
+  connection.query("SELECT * FROM actors", function(err, result) {
+    if (err) throw err;
+    var html = "<h1>Actors</h1>";
+    html += "<ul>";
+    for (var i = 0; i < result.length; i++) {
+      html += "<li><p> ID: " + result[i].id + "</p>";
+      html += "<li><p> Name: " + result[i].name + "</p>";
+      html += "<li><p> Coolness: " + result[i].coolness + "</p>";
+      html += "<li><p> Attitude: " + result[i].attitude + "</p></li>";
+    }
+    html += "</ul>";
+    res.send(html);
+  });
+});
+
+app.get("/coolness-chart", (req, res) => {
+  connection.query("SELECT * FROM actors ORDER BY coolness DESC", function(err, result) {
+    if (err) throw err;
+    var html = "<h1>Actors</h1>";
+    html += "<ul>";
+    for (var i = 0; i < result.length; i++) {
+      html += "<li><p> ID: " + result[i].id + "</p>";
+      html += "<li><p> Name: " + result[i].name + "</p>";
+      html += "<li><p> Coolness: " + result[i].coolness + "</p>";
+      html += "<li><p> Attitude: " + result[i].attitude + "</p></li>";
+    }
+    html += "</ul>";
+    res.send(html);
+  });
+});
+
+app.get("/attitude-chart/:att", (req, res) => {
+  const attitude = req.params.att;
+  connection.query(`SELECT * FROM actors WHERE attitude = "${attitude}"`, function(err, result) {
+    if (err) throw err;
+    var html = "<h1>Actors</h1>";
+    html += "<ul>";
+    for (var i = 0; i < result.length; i++) {
+      html += "<li><p> ID: " + result[i].id + "</p>";
+      html += "<li><p> Name: " + result[i].name + "</p>";
+      html += "<li><p> Coolness: " + result[i].coolness + "</p>";
+      html += "<li><p> Attitude: " + result[i].attitude + "</p></li>";
+    }
+    html += "</ul>";
+    res.send(html);
+  });
+});
+
+app.listen(PORT, () => console.log("express server listening on", PORT));
