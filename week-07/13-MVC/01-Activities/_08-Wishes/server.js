3e0007638a003d2178432a6b3d219b39e952f337
Ari Tserendavaa <tserenda@hotmail.com>
2020-01-15 20:16:04 -0800

    reviewed

--- week-07/13-MVC/01-Activities/_08-Wishes/server.js
+++ week-07/13-MVC/01-Activities/_08-Wishes/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const mysql = require("mysql");
-const exphbs = require("express-handlebars");
+const handlebars = require("express-handlebars");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.use(express.urlencoded({ extended: true }));
@@ -26,10 +26,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-  const myWish = req.body.makeWish;
-  console.log(myWish);
-
-  connection.query("INSERT INTO wish (wish) VALUES ('" + myWish + "')", (err, data) => res.redirect("/"));
+  connection.query("INSERT INTO wish (wish) VALUES ('" + req.body.make_wish + "')", (err, data) => res.redirect("/"));
 });
 
 app.listen(PORT, () => console.log("express server listening on http://localhost:" + PORT));
