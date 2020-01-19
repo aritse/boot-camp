dcd3fb2a518c97ff2e016f257ecaa21e7f673653
Ari Tserendavaa <tserenda@hotmail.com>
2020-01-18 19:43:45 -0800

    reviewed the http server

--- week-06/11-express/01-Activities/06-Request-Methods/server.js
+++ week-06/11-express/01-Activities/06-Request-Methods/server.js
@@ -1,33 +1,17 @@
-// Dependencies
-var http = require("http");
-
-var PORT = 8080;
-
-var server = http.createServer(handleRequest);
+const http = require("http");
+const PORT = 3000;
+const server = http.createServer(handleRequest); // create server
+server.listen(PORT, () => console.log("http server listening on", PORT)); // start server
 
 function handleRequest(req, res) {
-
-  // Saving the request data as a variable
-  var requestData = "";
-
+  // save the request data as a variable
+  let request = "";
   // When the server receives data...
-  req.on("data", function(data) {
-
-    // Add it to requestData.
-    requestData += data;
-  });
-
+  req.on("data", data => (request += data));
   // When the request has ended...
-  req.on("end", function() {
-
-    // Log (server-side) the request method, as well as the data received!
-    console.log("You did a", req.method, "with the data:\n", requestData);
+  req.on("end", () => {
+    // Log the request method and the data received
+    console.log("You did a", req.method, "with the data:\n", request);
     res.end();
   });
-
 }
-
-// Start our server
-server.listen(PORT, function() {
-  console.log("Server listening on: http://localhost:" + PORT);
-});
