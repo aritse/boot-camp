401cdcfe21d9d7d5505275b0c6aea031304f82cb
Ari Tserendavaa <tserenda@hotmail.com>
2020-02-04 21:34:40 -0800

    bundled with webpack

--- week-09/18-PWA/01-Activities/20-Stu-PWA-refactor/Unsolved/server.js
+++ week-09/18-PWA/01-Activities/20-Stu-PWA-refactor/Unsolved/server.js
@@ -11,10 +11,7 @@ app.use(express.json());
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://heroku_bjt6g1q4:93gkpkv3gdv7tjmaarmpl4f7k2@ds157946.mlab.com:57946/heroku_bjt6g1q4",
-  { useNewUrlParser: true }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/imageperformance", { useNewUrlParser: true });
 
 app.listen(PORT, function() {
   console.log(`Now listening on port: ${PORT}`);
