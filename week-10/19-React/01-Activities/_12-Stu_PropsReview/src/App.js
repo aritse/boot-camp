76fe9f35d3202d92d44da89313cd5cde308c7ec2
Ari Tserendavaa <tserenda@hotmail.com>
2020-02-10 21:19:30 -0800

    reviewed

---
+++ week-10/19-React/01-Activities/_12-Stu_PropsReview/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import FriendCard from "./components/FriendCard";
+import friends from "./friends.json";
+
+function App() {
+  return (
+    <Wrapper>
+      <Title>Friends List</Title>
+      {friends.map((friend, index) => (
+        <FriendCard key={index} name={friend.name} image={friend.image} occupation={friend.occupation} location={friend.location} />
+      ))}
+    </Wrapper>
+  );
+}
+
+export default App;
