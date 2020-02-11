b322e3c8001383ae0cd944a9dc9b3ed4beadb74c
Joseph Rehfuss <josephrehfuss@D-172-22-159-37.dhcp4.washington.edu>
2020-02-11 14:48:08 -0800

    adding tuesday stuff

---
+++ week-11/21-MERN/joesActivites/05-Stu_ReactRouter/Unsolved/client/src/pages/Books.js
@@ -0,0 +1,123 @@
+import React, { useState, useEffect } from "react";
+import DeleteBtn from "../components/DeleteBtn";
+import Jumbotron from "../components/Jumbotron";
+import API from "../utils/API";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../components/Grid";
+import { List, ListItem } from "../components/List";
+import { Input, TextArea, FormBtn } from "../components/Form";
+
+function Books() {
+  const [books, setBooks] = useState([])
+  const [formObject, setFormObject] = useState({
+    title: "",
+    author: "",
+    synopsis: ""
+  })
+
+  useEffect(() => {
+    loadBooks()
+  }, [])
+
+  function loadBooks() {
+    API.getBooks()
+      .then(res =>
+        setBooks(res.data)
+      )
+      .catch(err => console.log(err));
+  };
+
+  function deleteBook(id) {
+    API.deleteBook(id)
+      .then(res => loadBooks())
+      .catch(err => console.log(err));
+  }
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setFormObject({ ...formObject, [name]: value })
+  };
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    if (formObject.title && formObject.author) {
+      API.saveBook({
+        title: formObject.title,
+        author: formObject.author,
+        synopsis: formObject.synopsis
+      })
+        .then(res => {
+          setFormObject({
+            title: "",
+            author: "",
+            synopsis: ""
+          })
+          loadBooks()
+        })
+        .catch(err => console.log(err));
+    }
+  };
+
+  return (
+    <Container fluid>
+      <Row>
+        <Col size="md-6">
+          <Jumbotron>
+            <h1>What Books Should I Read?</h1>
+          </Jumbotron>
+          <form>
+            <Input
+              onChange={handleInputChange}
+              value={formObject.title}
+              name="title"
+              placeholder="Title (required)"
+            />
+            <Input
+              onChange={handleInputChange}
+              value={formObject.author}
+              name="author"
+              placeholder="Author (required)"
+            />
+            <TextArea
+              onChange={handleInputChange}
+              value={formObject.synopsis}
+              name="synopsis"
+              placeholder="Synopsis (Optional)"
+            />
+            <FormBtn
+              disabled={!(formObject.author && formObject.title)}
+              onClick={handleFormSubmit}
+            >
+              Submit Book
+              </FormBtn>
+          </form>
+        </Col>
+        <Col size="md-6 sm-12">
+          <Jumbotron>
+            <h1>Books On My List</h1>
+          </Jumbotron>
+          {books.length ? (
+            <List>
+              {books.map(book => (
+                <ListItem key={book._id}>
+                  {/* TODO: convert this a tag to a Link tag once react router is set up */}
+                  <a href={"/books/" + book._id}>
+                    <strong>
+                      {book.title} by {book.author}
+                    </strong>
+                  </a>
+                  <DeleteBtn onClick={() => deleteBook(book._id)} />
+                </ListItem>
+              ))}
+            </List>
+          ) : (
+              <h3>No Results to Display</h3>
+            )}
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+
+export default Books;
