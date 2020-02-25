f32fc3edef289948cc106ebe06b678050748c872
Ari Tserendavaa <tserenda@hotmail.com>
2020-02-24 18:54:02 -0800

    sync to upstream

--- week-12/22-Computer-Science/01-Activities/02-Binary-Search/Examples/binarySearch.js
+++ week-12/22-Computer-Science/01-Activities/02-Binary-Search/Examples/binarySearch.js
@@ -1,40 +1,19 @@
-var result = binarySearch([1, 23, 43, 56, 77, 89, 211, 212, 789, 972, 1001, 4567, 4599, 83784], 77);
+const result = binarySearch([1, 23, 43, 56, 77, 89, 211, 212, 789, 972, 1001, 4567, 4599, 83784], 77);
 console.log(result);
 
 function binarySearch(numbersArr, searchElement) {
 
   // Set some starting values.
-  var currentElement;
-  var currentIndex;
-  var maxIndex = numbersArr.length - 1;
-  var minIndex = 0;
+  const currentElement;
+  const currentIndex;
+  const [minIndex, maxIndex] = [0, numbersArr.length - 1];
 
-  // This is the main loop.
   while (minIndex <= maxIndex) {
-
-    // Get a position near the middle.
     currentIndex = Math.floor((minIndex + maxIndex) / 2);
-
-    // Get that element.
     currentElement = numbersArr[currentIndex];
-
-    // Test it.
-    if (currentElement < searchElement) {
-      // if it's less than we are looking for, look *above* this value.
-      minIndex = currentIndex + 1;
-
-    }
-    else if (currentElement > searchElement) {
-
-      // If it's more than we are looking for, look *below* this value.
-      maxIndex = currentIndex - 1;
-    }
-    else {
-      // We found it; return the index.
-      return currentIndex;
-    }
-
+    if (currentElement < searchElement) minIndex = currentIndex + 1;
+    else if (currentElement > searchElement) maxIndex = currentIndex - 1;
+    else return currentIndex;
   }
-
   return false;
 }
