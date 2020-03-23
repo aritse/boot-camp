bfd7f27c2578a4d5457231e6b1d04bc3e7e09e45
Ari Tserendavaa <tserenda@hotmail.com>
2020-03-22 19:11:15 -0700

    reviewed

--- week-10/19-React/01-Activities/14-Ins_BasicState/src/components/Counter.js
+++ week-10/19-React/01-Activities/14-Ins_BasicState/src/components/Counter.js
@@ -17,11 +17,15 @@ class Counter extends React.Component {
   render() {
     return (
       <div className="card text-center">
-        <div className="card-header bg-primary text-white">
-          Click Counter!
-        </div>
+        <div className="card-header bg-primary text-white">Click Counter!</div>
         <div className="card-body">
           <p className="card-text">Click Count: {this.state.count}</p>
+          {/* <button
+            className="btn btn-primary"
+            onClick={() => {
+              this.setState({ count: this.state.count + 1 });
+            }}
+          > */}
           <button className="btn btn-primary" onClick={this.handleIncrement}>
             Increment
           </button>
