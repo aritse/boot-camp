f040b39a845fdf26f002c7ea0afa2138daf6752d
Ari Tserendavaa <tserenda@hotmail.com>
2020-04-05 10:21:05 -0700

    static items are declared with .prototype property

--- week-05/10-OOP/01-Activities/05-Ins_Prototypes/prototype-demo.js
+++ week-05/10-OOP/01-Activities/05-Ins_Prototypes/prototype-demo.js
@@ -8,7 +8,7 @@ const myArray = [2, 4, 6, 8];
 myObject = {
   name: "Eric",
   age: 28,
-  occupation: "Curriculum Dev"
+  occupation: "Curriculum Dev",
 };
 // console.log(myObject);
 // console.log("Hello");
@@ -29,7 +29,8 @@ function Movie(title, releaseYear) {
 // prototype is a property of the class definition. That means prototype is a static property. You access prototype via class name instead
 // of an object instance. As class' static property, it has access to instance variables. The moral of the story is that you can define
 // static functions that are shared between object instances. That's why, all arrays have forEach(), map(), reduce(), and filter() methods.
-Movie.prototype.logInfo = function() {
+// Basically, you can define static property and methods with .prototype property.
+Movie.prototype.logInfo = function () {
   console.log(`${this.title} was released in ${this.releaseYear}`);
 };
 
@@ -39,4 +40,6 @@ console.log("object has title:", theShining.hasOwnProperty("title")); //true
 console.log("object has releaseYear:", theShining.hasOwnProperty("releaseYear")); //true
 console.log("object has log info():", theShining.hasOwnProperty("logInfo")); //false
 console.log("class has prototype:", Movie.hasOwnProperty("prototype")); //true
+console.log("class has title:", Movie.hasOwnProperty("title")); //false
 console.log("prototype of class has logInfo:", Movie.prototype.hasOwnProperty("logInfo")); //true
+console.log("\nClass", Movie);
