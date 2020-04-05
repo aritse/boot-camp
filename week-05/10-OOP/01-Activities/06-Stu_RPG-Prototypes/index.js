93c7ac7cda4d4a5437b632433c8d26d88c1536f4
Ari Tserendavaa <tserenda@hotmail.com>
2020-04-05 11:13:28 -0700

    reviewed and improved the logic

--- week-05/10-OOP/01-Activities/06-Stu_RPG-Prototypes/index.js
+++ week-05/10-OOP/01-Activities/06-Stu_RPG-Prototypes/index.js
@@ -4,47 +4,48 @@ function Hero(name, profession, age, strength, hitPoints) {
   this.age = age;
   this.strength = strength;
   this.hitPoints = hitPoints;
-  this.printStats = function() {
-    console.log([this.name, this.profession, this.age, this.strength, this.hitPoints].join(" : "));
+  this.printStats = function () {
+    console.log(`${this.name} => Profession: ${this.profession}, Age: ${age}, Strength: ${this.strength}, Hitpoint: ${this.hitPoints}`);
   };
 }
 
-Hero.prototype.isAlive = function() {
-  console.log(this.name, "is", this.hitPoints > 0 ? "alive" : "dead");
+Hero.prototype.isAlive = function () {
+  return this.hitPoints > 0 ? true : false;
 };
 
-Hero.prototype.attack = function(char) {
+Hero.prototype.attack = function (char) {
   console.log(this.name, "is attacking", char.name);
   char.hitPoints = Math.max(0, char.hitPoints - this.strength);
 };
 
-Hero.prototype.levelUp = function() {
+Hero.prototype.levelUp = function () {
   console.log(this.name, "is leveling up...");
-  this.age++;
+  this.age--;
   this.strength += 5;
-  this.hitPoints += 25;
+  this.hitPoints = 500;
 };
 
-const superman = new Hero("Super Woman", "Lawyer", 25, 20, 100);
-const batman = new Hero("Bat Man", "Doctor", 30, 30, 100);
+console.log("Spawning fighters ...");
 
-superman.printStats();
-batman.printStats();
+const red = new Hero("Hulk", "Doctor", 30, 30, 500);
+const blue = new Hero("Batman", "Lawyer", 30, 30, 500);
 
-superman.isAlive();
-batman.isAlive();
+red.printStats();
+blue.printStats();
 
-superman.attack(batman);
-superman.attack(batman);
-superman.attack(batman);
-superman.attack(batman);
-superman.attack(batman);
-superman.attack(batman);
+for (let i = 0; i < 3; i++) {
+  while (blue.isAlive() && red.isAlive()) {
+    Math.floor(Math.random() * 10) % 2 === 0 ? red.attack(blue) : blue.attack(red);
+  }
 
-superman.printStats();
-batman.printStats();
+  red.printStats();
+  blue.printStats();
 
-batman.isAlive();
-batman.levelUp();
-superman.printStats();
-batman.printStats();
+  if (red.isAlive()) {
+    console.log(blue.name, "is dead.");
+    blue.levelUp();
+  } else {
+    console.log(red.name, "is dead.");
+    red.levelUp();
+  }
+}
