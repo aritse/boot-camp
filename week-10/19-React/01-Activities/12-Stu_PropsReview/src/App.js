e09c1744663f7fe7aa734afc0b38e451be7ec1eb
Ari Tserendavaa <tserenda@hotmail.com>
2020-12-08 22:17:59 -0800

    Review

--- week-10/19-React/01-Activities/12-Stu_PropsReview/src/App.js
+++ week-10/19-React/01-Activities/12-Stu_PropsReview/src/App.js
@@ -1,18 +1,13 @@
-import React from "react";
-import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
-import FriendCard from "./components/FriendCard";
 import friends from "./friends.json";
+import Wrapper from "./components/Wrapper";
+import FriendCard from "./components/FriendCard";
+import Title from "./components/Title";
 
-function App() {
+export default function App() {
   return (
     <Wrapper>
       <Title>Friends List</Title>
-      {friends.map((friend, index) => (
-        <FriendCard key={index} name={friend.name} image={friend.image} occupation={friend.occupation} location={friend.location} />
-      ))}
+      {friends.map((f, i) => <FriendCard key={i} name={f.name} image={f.image} occupation={f.occupation} location={f.location} />)}
     </Wrapper>
   );
 }
-
-export default App;
--- week-10/19-React/01-Activities/12-Stu_PropsReview/src/components/FriendCard/index.js
+++ week-10/19-React/01-Activities/12-Stu_PropsReview/src/components/FriendCard/index.js
@@ -1,27 +1,18 @@
-import React from "react";
 import "./style.css";
 
-function FriendCard(props) {
+export default function FriendCard(props) {
   return (
     <div className="card">
       <div className="img-container">
-        <img alt={props.name} src={props.image} />
+        <img alt={props.name} src={props.image}></img>
       </div>
       <div className="content">
         <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
-          </li>
-          <li>
-            <strong>Occupation:</strong> {props.occupation}
-          </li>
-          <li>
-            <strong>Location:</strong> {props.location}
-          </li>
+          <li><strong>Name:</strong> {props.name}</li>
+          <li><strong>Occupation:</strong> {props.occupation}</li>
+          <li><strong>Location:</strong> {props.location}</li>
         </ul>
       </div>
     </div>
   );
 }
-
-export default FriendCard;
