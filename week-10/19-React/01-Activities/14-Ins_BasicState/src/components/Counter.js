a2790b4c7af9f67824643337b5df7418ed7a0b6e
Ari Tserendavaa <tserenda@hotmail.com>
2021-03-03 17:58:52 -0800

    review

--- week-10/19-React/01-Activities/14-Ins_BasicState/src/components/Counter.js
+++ week-10/19-React/01-Activities/14-Ins_BasicState/src/components/Counter.js
@@ -1,16 +1,19 @@
-import {useState} from "react";
+import { useState } from 'react';
 
 export default function Counter() {
   const [count, setCount] = useState(0);
-  const onClickHandler = () => setCount(count + 1);
+
+  const onButtonClick = () => {
+    setCount(count + 1);
+  }
 
   return (
-    <div className="card text-center">
-      <div className="card-header bg-primary text-white">Click Counter</div>
-      <div classname="card-body">
-        <p className="card-text">Count: {count}</p>
-        <button className="btn btn-primary" onClick={onClickHandler}>Increment</button>
+    <div className='card text-center'>
+      <div className='card-header bg-primary text-white'>Click Counter</div>
+      <div className='card-body'>
+        <p className='card-text'>Count: {count}</p>
+        <button className='btn btn-primary' onClick={onButtonClick}>Increment</button>
       </div>
     </div>
-  );
+  )
 }
